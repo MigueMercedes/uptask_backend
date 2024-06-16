9da7a1985996b5065bed2e5c35c1fe5478dc3efa
Miguel Mercedes <lmyguel0324@gmail.com>
2024-06-15 22:30:57 -0400

    feat: Add FRONTEND_URL environment variable
    This commit adds the FRONTEND_URL environment variable to the .env.example file and updates the package.json and server.ts files to include the necessary dependencies and configurations for CORS. This change allows the frontend application to make requests to the backend API from a different origin.

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connectDB } from './config/db'
-import projectRoutes from './routes/project.routes'
-import taskRoutes from './routes/task.routes'
-// Allows use environment variables
-dotenv.config()
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { corsConfig } from './config/cors';
+import { connectDB } from './config/db';
+import projectRoutes from './routes/project.routes';
+import taskRoutes from './routes/task.routes';
 
-connectDB()
+// Allows use environment variables
+dotenv.config();
+
+// Connect to DB
+connectDB();
 
 // Creates express server
-const app = express()
+const app = express();
+
+// config CORS
+app.use(cors(corsConfig));
 
 // Allows read JSONs
-app.use(express.json())
+app.use(express.json());
 
 // Routes
-app.use('/api/projects', projectRoutes)
-app.use('/api/projects', taskRoutes)
+app.use('/api/projects', projectRoutes);
+app.use('/api/projects', taskRoutes);
 
 export default app;
